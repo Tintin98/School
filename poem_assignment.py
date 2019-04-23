e0433a947d3cc89b8d324b9496d99ae3e60fedb0
Tintin98 <47766656+Tintin98@users.noreply.github.com>
2019-04-22 22:02:02 -0700

    Add files via upload

---
+++ poem_assignment.py
@@ -0,0 +1,42 @@
+p1 = "Haikus are easy.\nBut sometimes they don't make sense.\nRefrigerator."
+p2 = "I think haikus suck.\nHas to be five seven five.\nWho came up with this?"
+p3 = "Five syllables here.\nSeven more syllables here.\nAre you happy now?"
+
+# print(haiku_1 + "\n\n" + haiku_2 + "\n\n" + haiku_3)
+# I kept this here in case I wanted to print all of the poems at once.
+
+class Poem:
+    obj_count = 0
+    def __init__(self, title, author, haiku_variable):
+        Poem.obj_count += 1
+        self.title = title
+        self.author = author
+        self.haiku_variable = haiku_variable
+
+    def print_text(self):
+        return self.haiku_variable
+
+    def setHaiku(self):
+        if len(self.haiku_variable) >= 1:
+            return True
+
+
+
+
+haiku_1 = Poem("Haiku 1", "unknown", p1)
+haiku_2 = Poem("Haiku 2", "unknown", p2) # Random haikus found online
+haiku_3 = Poem("Haiku 3", "unknown", p3) # No title or authors found
+
+print("The number of Poems created: ", str(Poem.obj_count), "\n")
+print(haiku_1.print_text())
+print("Is it a Haiku?: ", haiku_1.setHaiku(), "\n")
+print(haiku_2.print_text())
+print("Is it a Haiku?: ", haiku_2.setHaiku(), "\n")
+print(haiku_3.print_text())
+print("Is it a Haiku?: ", haiku_3.setHaiku(), "\n")
+
+
+
+
+
+
